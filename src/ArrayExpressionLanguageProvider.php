76143084a90a2f8f500f51ea00684f65e83494fd
sebprt <sebastien.parrat@orange.fr>
2021-05-03 11:57:55 +0200

    rename function

--- src/ArrayExpressionLanguageProvider.php
+++ src/ArrayExpressionLanguageProvider.php
@@ -10,7 +10,7 @@ class ArrayExpressionLanguageProvider implements ExpressionFunctionProviderInter
     public function getFunctions(): array
     {
         return array(
-            ExpressionFunction::fromPhp('implode')
+            ExpressionFunction::fromPhp('implode', 'join')
         );
     }
 }
