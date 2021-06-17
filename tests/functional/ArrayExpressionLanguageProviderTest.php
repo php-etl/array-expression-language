ad2bad38bf64520993d0b337f7f3ff6f825ee73e
sebprt <sebastien.parrat@orange.fr>
2021-06-17 17:02:35 +0200

    Added phpunit and tests

---
+++ tests/functional/ArrayExpressionLanguageProviderTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace functional\Kiboko\Component\ArrayExpressionLanguage;
+
+use Kiboko\Component\ArrayExpressionLanguage\ArrayExpressionLanguageProvider;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
+
+class ArrayExpressionLanguageProviderTest extends TestCase
+{
+    public function testCountExpression(): void
+    {
+        $input = [
+            'bar',
+            'foo'
+        ];
+
+        $interpreter = new ExpressionLanguage(null, [new ArrayExpressionLanguageProvider()]);
+
+        $this->assertEquals(2, $interpreter->evaluate('count(input)', ['input' => $input]));
+    }
+
+    public function testListExpression(): void
+    {
+        $input = [];
+
+        $interpreter = new ExpressionLanguage(null, [new ArrayExpressionLanguageProvider()]);
+
+        $this->assertEquals(
+            [
+                'test'
+            ],
+            $interpreter->evaluate('list(1, "test")', ['input' => $input])
+        );
+    }
+
+    public function testFirstKeyExpression(): void
+    {
+        $input = [
+            'first_name' => 'Wouter',
+            'lastName' => 'de Jond',
+        ];
+
+        $interpreter = new ExpressionLanguage(null, [new ArrayExpressionLanguageProvider()]);
+
+        $this->assertEquals(
+            'first_name',
+            $interpreter->evaluate('firstKey(input)', ['input' => $input])
+        );
+    }
+
+    public function testLastKeyExpression(): void
+    {
+        $input = [
+            'first_name' => 'Wouter',
+            'lastName' => 'de Jond',
+        ];
+
+        $interpreter = new ExpressionLanguage(null, [new ArrayExpressionLanguageProvider()]);
+
+        $this->assertEquals(
+            'lastName',
+            $interpreter->evaluate('lastKey(input)', ['input' => $input])
+        );
+    }
+
+    public function testIterableToArrayExpression(): void
+    {
+        $input = new \ArrayIterator([
+            [
+                'first_name' => 'Wouter',
+                'lastName' => 'de Jond',
+            ],
+            [
+                'first_name' => 'Foo',
+                'lastName' => 'Bar',
+            ]
+        ]);
+
+        $interpreter = new ExpressionLanguage(null, [new ArrayExpressionLanguageProvider()]);
+
+        $this->assertEquals(
+            [
+                [
+                    'first_name' => 'Wouter',
+                    'lastName' => 'de Jond',
+                ],
+                [
+                    'first_name' => 'Foo',
+                    'lastName' => 'Bar',
+                ]
+            ],
+            $interpreter->evaluate('iterableToArray(input)', ['input' => $input])
+        );
+    }
+}
